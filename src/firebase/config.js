b89ce2faf0961a6f2d193cf535f843e4a90acb02
Prince Raj <princeraj2403i@gmail.com>
2026-06-24 12:58:56 +0530

    Use environment variables for Firebase config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,20 +2,19 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA2Nyg1_u99OVYqkXbxd96d9gfZisfe278",
-  authDomain: "ninja-firegram-7d366.firebaseapp.com",
-  projectId: "ninja-firegram-7d366",
-  storageBucket: "ninja-firegram-7d366.firebasestorage.app",
-  messagingSenderId: "235539565310",
-  appId: "1:235539565310:web:b0271b791ab2698e65b3af"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
-// Initialize Firebase
+
 firebase.initializeApp(firebaseConfig);
 
 const projectFirestore = firebase.firestore();
 const projectAuth = firebase.auth();
 const timestamp = firebase.firestore.FieldValue.serverTimestamp;
 
-export{ projectFirestore, projectAuth, timestamp };
+export { projectFirestore, projectAuth, timestamp };
